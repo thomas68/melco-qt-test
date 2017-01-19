78d69da42a93223d5f787ff2726ee1305616ed75
Thomas <thomas.johansson@melcogot.com>
2017-01-19 21:50:19 +0000

    test project

---
+++ HelloTest/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    std::cout << "Hello World" << std::endl;
+
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
